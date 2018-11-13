dfa8bbcc46008d80e4fb01328d9eee641f6844d7
7k <7k@iMac-de-administrador-2.local>
2018-11-13 08:07:32 -0600

    First Commit

---
+++ src/HomeView.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    View,
+} from 'react-native';
+import ucol from './assets/ucol.png';
+
+import ArtistList from './ArtistList'
+import {getMusicData} from './api-client'
+
+export default class HomeView extends Component<Props>{
+    state = {
+        artists: null
+    }
+    componentDidMount() {
+        getMusicData().then(data => this.setState({artists: data}))
+    }
+
+    render(){
+        const artists = this.state.artists
+
+        if(artists)
+            console.warn(artists[0].name)
+
+        return(
+            <View style = {styles.container}>
+                {artists && <ArtistList artists={artists} />}
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        backgroundColor: 'lightgray',
+    }
+});
